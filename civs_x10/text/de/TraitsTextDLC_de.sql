64e63f452de5a57f6c5b3960483c8c902469eb63
Michl <michel@michel-Desktop.fritz.box>
2017-09-24 08:42:46 +0200

    changed Nubia to german

--- civs_x10/text/de/TraitsTextDLC_de.sql
+++ civs_x10/text/de/TraitsTextDLC_de.sql
@@ -171,23 +171,23 @@ AND Tag = 'LOC_TRAIT_LEADER_FALL_BABYLON_DESCRIPTION';
 --Ta Seti
 UPDATE LocalizedText
 SET Text = '+500% [ICON_Production] Produktion für Fernkampfeinheiten. Alle Fernkampfeinheiten erhalten +500% Kampferfahrung. Minen auf strategischen Ressourcen liefern +10 [ICON_Production] Produktion. Minen auf Bonus-Ressourcen und Luxusgütern liefern +20 [ICON_Gold] Gold.'
-WHERE Language = 'en_US'
+WHERE Language = 'de_DE'
 AND Tag = 'LOC_TRAIT_CIVILIZATION_TA_SETI_DESCRIPTION';
 
 --Pyramid
 UPDATE LocalizedText
 SET Text = 'Modernisierung, die durch Steinmetzkunst freigeschaltet wird und auf Wüste, Wüstenhügeln oder Schwemmland gebaut werden muss. +10 [ICON_Faith] Glauben. Erhält zusätzliche Erträge durch angrenzende Bezirke. +10 [ICON_Food] Nahrung, wenn angrenzend an ein Stadtzentrum. Für alle anderen Bezirke, die Nachbarschaftsbonusse gewähren: +10 für den entsprechenden Ertrag, wenn der Bezirk angrenzt.'
-WHERE Language = 'en_US'
+WHERE Language = 'de_DE'
 AND Tag = 'LOC_IMPROVEMENT_PYRAMID_DESCRIPTION';
 
 --UU Pitati
 UPDATE LocalizedText
 SET Text = 'Teure nubische Spezialeinheit der Antike, die den Bogenschützen ersetzt. Viel Stärker als der Bogenschütze mit zusätzlicher 10 [ICON_Movement] Fortbewegung.'
-WHERE Language = 'en_US'
+WHERE Language = 'de_DE'
 AND Tag = 'LOC_UNIT_NUBIAN_PITATI_DESCRIPTION';
 
 --Aminatore
 UPDATE LocalizedText
 SET Text = '+200% [ICON_Production] Produktion für alle Bezirke oder +400%, wenn eine Nubische Pyramide an das Stadtzentrum angrenzt'
-WHERE Language = 'en_US'
+WHERE Language = 'de_DE'
 AND Tag = 'LOC_TRAIT_LEADER_KANDAKE_OF_MEROE_DESCRIPTION';
